32fa797c76dbc0dba430865a35e744e2af52bf30
kclifford281 <kevinclifford@hotmail.com>
2014-11-14 10:43:19 -0800

    My changes to cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 	Caching the Inverse of a Matrix
+## Matrix inversion is usually a costly computation and their may be some benefit to caching 
+## the inverse of a matrix rather than compute it repeatedly 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
+
+## makeCacheMatrixr creates a list containing functions to
+## 	1.set the value of the matrix
+##	2.get the value of the matrix
+##	3.set the value of the inverted matrix
+##	4.get the value of the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+
+## Calculates the inverse of the special matrix created with the above function. 
+## However, it first checks to see if the inverse has already been calculated. If so, 
+## it gets the inverse matrix from the cache and skips the computation. Otherwise, it 
+## calculates the inverse of the data and sets the value of the inverse in the cache via 
+## the setinverse function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
